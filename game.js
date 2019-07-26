18614e3c7868fe353ec15e7efa430f55b56d72ec
Ting <wisezt@hotmail.com>
2019-07-26 16:54:26 -0500

    Add hide function to all the buttons.

--- game.js
+++ game.js
@@ -1,12 +1,90 @@
-var gamePattern = [];
+$(document).ready(
+function(){
 
-function nextSequence(){
-  var randomNumber = Math.floor(Math.random()*4);
-  return randomNumber;
+
+
+
+  var gamePattern = [];
+
+  function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    return randomNumber;
+  }
+
+  var buttonColours = ["red", "blue", "green", "yellow"];
+
+  var randomChosenColour = buttonColours[nextSequence()];
+
+  gamePattern.push(randomChosenColour);
+
+
+  $("#green").on("click",
+  function(){
+    chooseButton("green")
+  });
+
+  $("#red").on("click",
+  function(){
+    chooseButton("red")
+  });
+
+  $("#yellow").on("click",
+  function(){
+    chooseButton("yellow")
+  });
+
+  $("#blue").on("click",
+  function(){
+    chooseButton("blue")
+  });
+
+
+
+  // $("#red").on("click", chooseButton("red"));
+  // $("#yellow").on("click", chooseButton("yellow"));
+  // $("#blue").on("click", chooseButton("blue"));
+
+
+  var chooseButton = function(buttonColour) {
+    switch (buttonColour) {
+      case "green":
+        $("#green").hide();
+
+
+        break;
+
+      case "red":
+      $("#red").hide();
+
+        break;
+
+      case "yellow":
+      $("#yellow").hide();
+
+        break;
+
+      case "blue":
+      $("#blue").hide();
+
+        break;
+
+
+
+
+
+
+      default:
+
+    }
+
+
+
+  }
+
+
+
+
+
 }
 
-var buttonColurs= ["red", "blue", "green", "yellow"];
-
-var randomChosenColour = buttonColurs[nextSequence()];
-
-gamePattern.push(randomChosenColour);
+);
