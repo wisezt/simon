0085038d3cfd1af2f063c90beaca805acd5a6058
Ting <wisezt@hotmail.com>
2019-07-26 17:26:36 -0500

    Add fadeOut and fadeIn abilities to the button

--- game.js
+++ game.js
@@ -1,90 +1,85 @@
 $(document).ready(
-function(){
+  function() {
 
 
 
 
-  var gamePattern = [];
+    var gamePattern = [];
 
-  function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
-  }
+    function nextSequence() {
+      var randomNumber = Math.floor(Math.random() * 4);
+      return randomNumber;
+    }
 
-  var buttonColours = ["red", "blue", "green", "yellow"];
+    var buttonColours = ["red", "blue", "green", "yellow"];
 
-  var randomChosenColour = buttonColours[nextSequence()];
+    var randomChosenColour = buttonColours[nextSequence()];
 
-  gamePattern.push(randomChosenColour);
-
-
-  $("#green").on("click",
-  function(){
-    chooseButton("green")
-  });
-
-  $("#red").on("click",
-  function(){
-    chooseButton("red")
-  });
-
-  $("#yellow").on("click",
-  function(){
-    chooseButton("yellow")
-  });
-
-  $("#blue").on("click",
-  function(){
-    chooseButton("blue")
-  });
+    gamePattern.push(randomChosenColour);
 
 
+    $("#green").on("click",
+      function() {
+        chooseButton("green")
+      });
 
-  // $("#red").on("click", chooseButton("red"));
-  // $("#yellow").on("click", chooseButton("yellow"));
-  // $("#blue").on("click", chooseButton("blue"));
+    $("#red").on("click",
+      function() {
+        chooseButton("red")
+      });
 
+    $("#yellow").on("click",
+      function() {
+        chooseButton("yellow")
+      });
 
-  var chooseButton = function(buttonColour) {
-    switch (buttonColour) {
-      case "green":
-        $("#green").hide();
-
-
-        break;
-
-      case "red":
-      $("#red").hide();
-
-        break;
-
-      case "yellow":
-      $("#yellow").hide();
-
-        break;
-
-      case "blue":
-      $("#blue").hide();
-
-        break;
+    $("#blue").on("click",
+      function() {
+        chooseButton("blue")
+      });
 
 
 
+    // $("#red").on("click", chooseButton("red"));
+    // $("#yellow").on("click", chooseButton("yellow"));
+    // $("#blue").on("click", chooseButton("blue"));
 
 
+    var chooseButton = function(buttonColour) {
+      switch (buttonColour) {
+        case "green":
+          $("#green").fadeOut(100).fadeIn(100);
+          break;
 
-      default:
+        case "red":
+          $("#red").fadeOut(100).fadeIn(100);
+          break;
+
+        case "yellow":
+          $("#yellow").fadeOut(100).fadeIn(100);
+          break;
+
+        case "blue":
+          $("#blue").fadeOut(100).fadeIn(100);
+          break;
+
+        default:
+
+      }
 
     }
 
 
 
+
+
+
+
+
+
+
+
+
   }
-
-
-
-
-
-}
 
 );
